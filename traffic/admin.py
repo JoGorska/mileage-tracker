b57805d26fc8dd7832934edb9d01a24cc1076977
Joanna Gorska <gorska919@gmail.com>
2021-12-15 21:39:48 +0000

    Add views for traffic messages

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import TrafficMessage
 
+
 @admin.register(TrafficMessage)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'area', 'county', 'category', 'created_on', 'status')
     search_fields = ['area', 'content']
     list_filter = ('status', 'created_on')
-    pass
--- traffic/views.py
+++ traffic/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import TrafficMessage
 
-# Create your views here.
+
+class PostMessages(generic.ListView):
+    model = TrafficMessage
+    queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 4
+
