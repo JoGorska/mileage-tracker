33591850315c750130da2ef2eb2337d92561a258
Joanna Gorska <gorska919@gmail.com>
2021-12-15 21:20:49 +0000

    Add trafficmessage database to admin page and add filters and sorting so managing the messages is easier

--- traffic/models.py
+++ traffic/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+# from cloudinary.models import CloudinaryField ??? might not need it
 
 STATUS_CHOICES = ((0, "Draft"), (1, "Published"))
 
