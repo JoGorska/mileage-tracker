380076a7282573a61ece7c9a72d1b7f0bcf9d6c7
Joanna Gorska <gorska919@gmail.com>
2021-12-16 23:04:51 +0000

    Add method to get thanks and cleared if user is logged in in views in traffic app

--- traffic/views.py
+++ traffic/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import TrafficMessage
 
@@ -8,3 +8,25 @@ class TrafficMessagesList(generic.ListView):
     queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+    # attempt to add functionality to add thanks and cleared
+
+    def get(self, request, id, *args,**kwargs):
+        queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+        trafficmessage = get_object_or_404(queryset, id=id)
+        thanks = False
+        cleared = False
+        if trafficmessage.thanks.filter(id=self.request.user.id).exists():
+            thanks = True
+        if trafficmessage.cleared.filter(id=self.request.user.id).exists():
+            cleared = True
+        
+        return render(
+            request,
+            "index.html",
+            {
+                "trafficmessage": trafficmessage,
+                "thanks": thanks,
+                "cleared": cleared
+            },
+        )
