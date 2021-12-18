5b907a4f2e9ab3ef4dc9fb1f07991a855d73b351
Joanna Gorska <gorska919@gmail.com>
2021-12-18 21:17:52 +0000

    Fix problem with displaying home page

--- traffic/views.py
+++ traffic/views.py
@@ -11,22 +11,22 @@ class TrafficMessagesList(generic.ListView):
 
     # attempt to add functionality to add thanks and cleared
 
-    def get(self, request, id, *args, **kwargs):
-        queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
-        trafficmessage = get_object_or_404(queryset, id=id)
-        thanks = False
-        cleared = False
-        if trafficmessage.thanks.filter(id=self.request.user.id).exists():
-            thanks = True
-        if trafficmessage.cleared.filter(id=self.request.user.id).exists():
-            cleared = True
+    # def get(self, request, id, *args, **kwargs):
+    #     queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+    #     trafficmessage = get_object_or_404(queryset, id=id)
+    #     thanks = False
+    #     cleared = False
+    #     if trafficmessage.thanks.filter(id=self.request.user.id).exists():
+    #         thanks = True
+    #     if trafficmessage.cleared.filter(id=self.request.user.id).exists():
+    #         cleared = True
         
-        return render(
-            request,
-            "index.html",
-            {
-                "trafficmessage": trafficmessage,
-                "thanks": thanks,
-                "cleared": cleared
-            },
-        )
+    #     return render(
+    #         request,
+    #         "index.html",
+    #         {
+    #             "trafficmessage": trafficmessage,
+    #             "thanks": thanks,
+    #             "cleared": cleared
+    #         },
+    #     )
