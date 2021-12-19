8f3ab02d332917896dead482e3967d0c5cf83078
Joanna Gorska <gorska919@gmail.com>
2021-12-19 16:21:04 +0000

    Add views to display user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+# FormView
+# from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import (
+    UserForm,
+    # UserProfileForm,
+    # AuthForm,
+    )
+
+
+class RegisterUserView(CreateView):
+    template_name = 'register/register.html'
+    form_class = UserForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
