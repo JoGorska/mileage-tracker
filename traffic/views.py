95985e622bda68bb7ba1b3afe84850a6c71e6b52
Joanna Gorska <gorska919@gmail.com>
2021-12-21 21:06:09 +0000

    Add views to display details of a traffic message

--- traffic/views.py
+++ traffic/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.views import generic, View
 from .models import TrafficMessage
 
 
@@ -9,24 +9,25 @@ class TrafficMessagesList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
-    # attempt to add functionality to add thanks and cleared
 
-    # def get(self, request, id, *args, **kwargs):
-    #     queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
-    #     trafficmessage = get_object_or_404(queryset, id=id)
-    #     thanks = False
-    #     cleared = False
-    #     if trafficmessage.thanks.filter(id=self.request.user.id).exists():
-    #         thanks = True
-    #     if trafficmessage.cleared.filter(id=self.request.user.id).exists():
-    #         cleared = True
+class TrafficMsgDetail(View):
+
+    def get(self, request, id, *args, **kwargs):
+        queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+        trafficmessage = get_object_or_404(queryset, id=id)
+        thanks = False
+        cleared = False
+        if trafficmessage.thanks.filter(id=self.request.user.id).exists():
+            thanks = True
+        if trafficmessage.cleared.filter(id=self.request.user.id).exists():
+            cleared = True
         
-    #     return render(
-    #         request,
-    #         "index.html",
-    #         {
-    #             "trafficmessage": trafficmessage,
-    #             "thanks": thanks,
-    #             "cleared": cleared
-    #         },
-    #     )
+        return render(
+            request,
+            "traffic_msg_detail.html",
+            {
+                "trafficmessage": trafficmessage,
+                "thanks": thanks,
+                "cleared": cleared
+            },
+        )
