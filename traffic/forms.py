74b70e600bf975c48eaa09c471a203668f67db3f
Joanna Gorska <gorska919@gmail.com>
2021-12-22 23:03:06 +0000

    Add changing status to published when post is posted to database, one less field for the user

--- traffic/forms.py
+++ traffic/forms.py
@@ -6,4 +6,4 @@ class TrafficMessageForm(forms.ModelForm):
     class Meta:
         model = TrafficMessage
         # important - don't forget coma at the end of the list of fields
-        fields = ('area', 'county', 'content', 'category', 'status',)
+        fields = ('area', 'county', 'content', 'category', )
