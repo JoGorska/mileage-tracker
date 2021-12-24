6b1008ddad698931b5c2a46c8b9453102bfd11bd
Joanna Gorska <gorska919@gmail.com>
2021-12-24 01:52:22 +0000

    Add new model for date picker to be displayed for the admin

--- visits/admin.py
+++ visits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DatePicker
 
-# Register your models here.
+
+@admin.register(DatePicker)
+class DateAdmin(admin.ModelAdmin):
+    list_display = ('date_picked', )
