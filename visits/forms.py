6b735c124cd05050b9acc48f5ab00469967f9cd2
Joanna Gorska <gorska919@gmail.com>
2021-12-26 15:23:56 +0000

    Add JourneyForm class in forms.py

--- visits/forms.py
+++ visits/forms.py
@@ -4,7 +4,29 @@ from .models import DatePicker
 
 
 class JourneyForm(forms.ModelForm):
-    
+
+    class Meta:
+        model = Journey
+        fields = (
+            'date_of_journey',
+            'created_on',
+            'updated_on',
+
+            'address_start',
+
+            'postcode_start',
+            'latitude_start',
+            'longitude_start',
+
+            'address_destination',
+            'postcode_destination',
+            'latitude_destination',
+            'longitude_destination',
+
+            'driver',
+
+            'distance',
+        )
 
 
 class DateInput(forms.DateInput):
