d6dae0cc8cabc3ab659e12a0bfa5e68613417de6
Joanna Gorska <gorska919@gmail.com>
2021-12-28 14:48:42 +0000

    Commented out unsused sections of the code in map.html and mixins.py

--- visits/mixins.py
+++ visits/mixins.py
@@ -5,6 +5,11 @@ import re
 
 
 def extract_postcode(full_address):
+    '''
+    takes a string containing full adress and postcodes and returns postcode only
+    Regex from:
+    https://stackoverflow.com/questions/164979/regex-for-matching-uk-postcodes
+    '''
     result = ''.join([c for c in full_address if c.isupper()])
     matches = re.findall(r'([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})', full_address)
     list_of_matches = matches[0]
@@ -14,7 +19,7 @@ def extract_postcode(full_address):
 
 def Directions(*args, **kwargs):
     """
-    Handles directions from Google
+    Posts coordinates to google API and gets json response - directions
     created by following tutorial:
     https://www.youtube.com/watch?v=wCn8WND-JpU&t=8s
     """
@@ -39,31 +44,28 @@ def Directions(*args, **kwargs):
     if directions["status"] == "OK":
 
         route = directions["routes"][0]["legs"][0]
-        # this is a string, postcode can be taken out of it counting positions of letters from the end of the string
-        # postcodes can be shorter and longer:
-        # M3 3EH or SW1A 2AB
+        # this is a string with full address, postcode can be extracted
         origin = route["start_address"]
         destination = route["end_address"]
-        # route is an object consisiting of text and value
-        # route["distance"]["text"] - returns 444 km, while route["distance"]["value"] returns 444
-        # I recalculate km to miles
 
+        # distance in km converted to miles:
         distance = route["distance"]["value"]*0.000621371
+        # duration is displayed as a string, not number
         duration = route["duration"]["text"]
 
-        steps = [
-			[
-				s["distance"]["text"],
-				s["duration"]["text"],
-				s["html_instructions"],
+    #     steps = [
+		# 	[
+		# 		s["distance"]["text"],
+		# 		s["duration"]["text"],
+		# 		s["html_instructions"],
 
-			]
-			for s in route["steps"]]
+		# 	]
+		# 	for s in route["steps"]]
 
-    return {
-		"origin": origin,
-		"destination": destination,
-		"distance": distance,
-		"duration": duration,
-		"steps": steps
-		}
+    # return {
+		# "origin": origin,
+		# "destination": destination,
+		# "distance": distance,
+		# "duration": duration,
+		# "steps": steps
+		# }
