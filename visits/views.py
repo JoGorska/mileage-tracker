0abc760d6963b37470930e9ae50c6b0a25191f3e
Joanna Gorska <gorska919@gmail.com>
2021-12-29 11:45:28 +0000

    Un commented previous work on date_picker view and url

--- visits/views.py
+++ visits/views.py
@@ -93,57 +93,57 @@ class AddVisit(CreateView):
         return HttpResponseRedirect('/')
 
 
-# class DatePickerView(View):
-#     template_name = 'visits/date_picker.html'
-#     form_class = DatePickerForm
+class DatePickerView(View):
+    template_name = 'visits/date_picker.html'
+    form_class = DatePickerForm
 
-#     def get(self, request, *args, **kwargs):
-#         return render(
-#             request,
-#             'visits/date_picker.html',
-#             {
-#                 'date_picker_form': DatePickerForm()
-#             },
-#         )
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'visits/date_picker.html',
+            {
+                'date_picker_form': DatePickerForm()
+            },
+        )
 
-#     def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
 
-#         date_picker_form = DatePickerForm(data=request.POST)
-
-
-#         if date_picker_form.is_valid():
-
-#             date_picked_record = date_picker_form.save(commit=False)
-#             date_picked_record.save('date_picked')
-#             date_picked = request.POST.get('date_picked')
-#             print(date_picked_record)
+        date_picker_form = DatePickerForm(data=request.POST)
 
 
-#             date_string = str(date_picked)
+        if date_picker_form.is_valid():
 
-#             print(date_string)
+            date_picked_record = date_picker_form.save(commit=False)
+            date_picked_record.save('date_picked')
+            date_picked = request.POST.get('date_picked')
+            print(date_picked_record)
 
-#             # return reverse('date_view', args=[date_string])
 
-#         else:
-#             date_picker_form = DatePickerForm()
+            date_string = str(date_picked)
+
+            print(date_string)
+
+            # return reverse('date_view', args=[date_string])
+
+        else:
+            date_picker_form = DatePickerForm()
         
-#         # return HttpResponseRedirect('this_is_not_my_url')
+        # return HttpResponseRedirect('this_is_not_my_url')
         
-#         return redirect(reverse('date_view', args=[date_string]))
-#         # return HttpResponseRedirect(reverse('date_view', args=[date_string]))
+        return redirect(reverse('date_view', args=[date_string]))
+        # return HttpResponseRedirect(reverse('date_view', args=[date_string]))
 
-# class DateView(View):
-#     template_name = 'visits/visits_by_date.html'
-#     form_class = DatePickerForm
+class DateView(View):
+    template_name = 'visits/visits_by_date.html'
+    form_class = DatePickerForm
 
-#     def get(self, request, *args, **kwargs):
-#         # queryset = DatePicker.objects
-#         # date_picker = get_object_or_404(queryset, id=id)
-#         return render(
-#             request,
-#             'visits/visits_by_date.html',
-#             {
-#                 'date_picker_form': DatePickerForm()
-#             },
-#         )
+    def get(self, request, *args, **kwargs):
+        # queryset = DatePicker.objects
+        # date_picker = get_object_or_404(queryset, id=id)
+        return render(
+            request,
+            'visits/visits_by_date.html',
+            {
+                'date_picker_form': DatePickerForm()
+            },
+        )
