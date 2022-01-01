3f582561fa873669a7bb166ea34a971d8e97d17f
Joanna Gorska <gorska919@gmail.com>
2022-01-01 21:38:38 +0000

    Style: add experimental files to use bootstrap, but not working, Add vanila css to style map.css

--- visits/forms.py
+++ visits/forms.py
@@ -4,9 +4,18 @@ from .models import DatePicker
 
 
 class DateInput(forms.DateInput):
+    '''
+    class that ads input_type date to to the widget
+    '''
     input_type = 'date'
 
+
 class JourneyForm(forms.ModelForm):
+    '''
+    form to input the date of the journey on the map view,
+    the rest of data needed for Journey model are taken from the
+    variables that are already on map page
+    '''
 
     class Meta:
         model = Journey
@@ -19,6 +28,9 @@ class JourneyForm(forms.ModelForm):
 
 
 class DatePickerForm(forms.ModelForm):
+    '''
+    form to pick a date for a list of journeys to be displayed
+    '''
     class Meta:
         model = DatePicker
         # important - don't forget coma at the end of the list of fields
