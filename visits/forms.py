9c6a7f2f16977a03a237f151b66206625114c7bc
Joanna Gorska <gorska919@gmail.com>
2022-01-01 12:27:05 +0000

    Add widget to Journey Form so the date_of_journey field displays as date picker

--- visits/forms.py
+++ visits/forms.py
@@ -3,34 +3,19 @@ from .models import Journey
 from .models import DatePicker
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class JourneyForm(forms.ModelForm):
 
     class Meta:
         model = Journey
         fields = (
             'date_of_journey',
-            # 'created_on',
-            # 'updated_on',
-
-            # 'address_start',
-
-            # 'postcode_start',
-            # 'latitude_start',
-            # 'longitude_start',
-
-            # 'address_destination',
-            # 'postcode_destination',
-            # 'latitude_destination',
-            # 'longitude_destination',
-
-            # 'driver',
-
-            # 'distance',
         )
-
-
-class DateInput(forms.DateInput):
-    input_type = 'date'
+        widgets = {
+            'date_of_journey': DateInput(),
+        }
 
 
 class DatePickerForm(forms.ModelForm):
