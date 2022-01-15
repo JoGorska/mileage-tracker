c2efe6e989a24376e3b8b6e7c9f08803b7e833bb
Joanna Gorska <gorska919@gmail.com>
2022-01-15 22:17:07 +0000

    Add coloured tanks if the post was marked thank you by the user

--- traffic/views.py
+++ traffic/views.py
@@ -10,9 +10,60 @@ from django.contrib.auth.models import User
 
 class TrafficMessagesList(generic.ListView):
     model = TrafficMessage
-    queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')   
+    queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+    # for traffic_msg in queryset:
+    #     print(F'TRAFFIC MESSAGE THANKS {traffic_msg.thanks.all}')
     template_name = 'index.html'
     paginate_by = 6
+
+
+
+
+# class TrafficMessagesList(generic.ListView):
+#     def get(self, request, *args, **kwargs):
+#         model = TrafficMessage
+#         queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+#         template_name = 'index.html'
+#         paginate_by = 6
+#         user_request = self.request.GET.get("user.id")
+#         print(f'USER REQUEST {user_request}')
+
+#         # attempt to make tanks change colors
+#         for traffic_msg in queryset:
+#             traffic_message_object = get_object_or_404(queryset, id=traffic_msg.id)
+#             thanked = False
+#             # if traffic_message_object.thanks.filter(id=user_request.id).exists():
+#             #     thanked = True
+#         return super(TrafficMessagesList,self).get(request, *args, **kwargs)
+
+
+
+
+
+# class TrafficMessagesList(CreateView):
+#     def get(self, request, *args, **kwargs):
+#         model = TrafficMessage
+#         trafficmessage_list = TrafficMessage.objects.filter(status=1).order_by('-created_on')
+#         template_name = 'index.html'
+#         paginate_by = 6
+#         user_id = request.user.id
+#         # attempt to make tanks change colors
+#         for traffic_msg in trafficmessage_list:
+#             traffic_message_object = get_object_or_404(trafficmessage_list, id=traffic_msg.id)
+#             thanked = False
+#             if traffic_message_object.thanks.filter(id=request.user.id).exists():
+#                 thanked = True
+#         return render(
+#             request,
+#             'index.html',
+#             {
+#                 'trafficmessage_list': trafficmessage_list,
+#                 'thanked': thanked
+#             },
+#         )
+
+
+
 
 
 class AddNewTrafficMsg(CreateView):
