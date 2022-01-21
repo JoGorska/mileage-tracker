9ab2a830cf66b48f797025af4d14ece3deab0cc4
Joanna Gorska <gorska919@gmail.com>
2022-01-21 22:54:04 +0000

    Fix errors raised by pep8 validator

--- traffic/urls.py
+++ traffic/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.TrafficMessagesList.as_view(), name='home'),
     path('add_traffic_msg', views.AddNewTrafficMsg.as_view(),
          name='add_traffic_msg'),
-    path('thanks/<int:id>', views.MsgThanks.as_view(), name='msg_thanks'),
+    path('thanks/<int:msg_id>', views.MsgThanks.as_view(), name='msg_thanks'),
 ]
--- traffic/views.py
+++ traffic/views.py
@@ -1,72 +1,26 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from django.views.generic.edit import CreateView, FormView
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
 from .models import TrafficMessage
 from .forms import TrafficMessageForm
-from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
 
 
 class TrafficMessagesList(generic.ListView):
+    '''
+    class view enabling to display the list of traffic allerts with pagination
+    '''
     model = TrafficMessage
     queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
-    # for traffic_msg in queryset:
-    #     print(F'TRAFFIC MESSAGE THANKS {traffic_msg.thanks.all}')
     template_name = 'index.html'
     paginate_by = 6
 
 
-
-
-# class TrafficMessagesList(generic.ListView):
-#     def get(self, request, *args, **kwargs):
-#         model = TrafficMessage
-#         queryset = TrafficMessage.objects.filter(status=1).order_by('-created_on')
-#         template_name = 'index.html'
-#         paginate_by = 6
-#         user_request = self.request.GET.get("user.id")
-#         print(f'USER REQUEST {user_request}')
-
-#         # attempt to make tanks change colors
-#         for traffic_msg in queryset:
-#             traffic_message_object = get_object_or_404(queryset, id=traffic_msg.id)
-#             thanked = False
-#             # if traffic_message_object.thanks.filter(id=user_request.id).exists():
-#             #     thanked = True
-#         return super(TrafficMessagesList,self).get(request, *args, **kwargs)
-
-
-
-
-
-# class TrafficMessagesList(CreateView):
-#     def get(self, request, *args, **kwargs):
-#         model = TrafficMessage
-#         trafficmessage_list = TrafficMessage.objects.filter(status=1).order_by('-created_on')
-#         template_name = 'index.html'
-#         paginate_by = 6
-#         user_id = request.user.id
-#         # attempt to make tanks change colors
-#         for traffic_msg in trafficmessage_list:
-#             traffic_message_object = get_object_or_404(trafficmessage_list, id=traffic_msg.id)
-#             thanked = False
-#             if traffic_message_object.thanks.filter(id=request.user.id).exists():
-#                 thanked = True
-#         return render(
-#             request,
-#             'index.html',
-#             {
-#                 'trafficmessage_list': trafficmessage_list,
-#                 'thanked': thanked
-#             },
-#         )
-
-
-
-
-
 class AddNewTrafficMsg(CreateView):
+    '''
+    class view in get - gets the traffic_msg_form and in post - posts the form
+    and creates new traffic alert
+    '''
     template_name = 'traffic/add_traffic_msg.html'
     form_class = TrafficMessageForm
     success_url = 'home'
@@ -90,7 +44,7 @@ class AddNewTrafficMsg(CreateView):
 
             traffic_message = traffic_msg_form.save(commit=False)
             traffic_message.save()
-        
+
         else:
             traffic_msg_form = TrafficMessageForm()
 
@@ -98,12 +52,16 @@ class AddNewTrafficMsg(CreateView):
 
 
 class MsgThanks(View):
-    def post(self, request, id):
-        traffic_message = get_object_or_404(TrafficMessage, id=id)
+    '''
+    posts thanks when user clicks the button, this ads user to the list of
+    users that added thanks to particular traffic alert, if user double clicks
+    the thanks get canceled
+    '''
+    def post(self, request, msg_id):
+        traffic_message = get_object_or_404(TrafficMessage, id=msg_id)
         if traffic_message.thanks.filter(id=request.user.id).exists():
             traffic_message.thanks.remove(request.user)
         else:
             traffic_message.thanks.add(request.user)
 
         return HttpResponseRedirect('/')
-
