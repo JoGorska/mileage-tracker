7d0ed7b08df80948d6e1c5854ff6cf615a2d383f
Joanna Gorska <gorska919@gmail.com>
2022-01-21 23:19:24 +0000

    Fix too long lines and other minor errors

--- traffic/admin.py
+++ traffic/admin.py
@@ -4,6 +4,9 @@ from .models import TrafficMessage
 
 @admin.register(TrafficMessage)
 class MessageAdmin(admin.ModelAdmin):
+    '''
+    class enabling admin see the model with details and search fields
+    '''
     list_display = ('id', 'area', 'county', 'category', 'created_on', 'status')
     search_fields = ['area', 'content']
     list_filter = ('status', 'created_on')
