a137746df7697fa0c9d16128642a74641a1b3b6b
Joanna Gorska <gorska919@gmail.com>
2022-04-30 19:57:59 +0000

    Add views to render each error page

--- error_pages/views.py
+++ error_pages/views.py
@@ -7,4 +7,18 @@ def error_404(request, exception):
     '''
     view rendering the 404 error page
     '''
-    return render(request, 'error_responses/404.html')
+    return render(request, 'templates/404.html')
+
+
+def error_414(request, exception):
+    '''
+    view rendering the 404 error page
+    '''
+    return render(request, 'templates/414.html')
+
+
+def error_500(request, exception):
+    '''
+    view rendering the 404 error page
+    '''
+    return render(request, 'templates/500.html')
