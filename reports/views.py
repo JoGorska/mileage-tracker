06a886120a6d28a21b9d710226d2972594b26d73
Joanna Gorska <gorska919@gmail.com>
2022-05-06 21:56:23 +0000

    Add filter to get right list of data to the context

--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from visits.models import Journey
 
 # Create your views here.
 
@@ -17,3 +20,27 @@ class ReportView(View):
         date and the other for end date
         '''
         return render(request, 'reports/dates.html')
+
+    def post(self, request):
+        '''
+        posts dates and filters data to be displayed in the table
+        '''
+        # gets data posted by the html form
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        user_id = request.POST.get('user_id')
+        # gets user object
+        user = get_object_or_404(User, id=user_id)
+
+        # filter visits by:
+        # - user
+        # - dates
+        all_journeys_in_period = (
+            Journey.objects
+            .filter(date_of_journey__range=[start_date, end_date])
+            .filter(driver=user))
+        print(f'JORUNEY DATE {journey.date for journey in all_journeys_in_period}')
+        context = {
+            'all_journeys_in_period': all_journeys_in_period,
+        }
+        return render(request, 'reports/table.html', context)
