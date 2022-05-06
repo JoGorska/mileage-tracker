1773a52e7259fa7aa5b6147132472c77eb494b3b
Joanna Gorska <gorska919@gmail.com>
2022-05-06 21:26:00 +0000

    Add urls, views and template to display date pickers for report

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class ReportView(View):
+    '''
+    In get displys date picker to pick start date and end date
+    of the report
+    In post makes query for data from journeys model
+    '''
+
+    def get(self, request):
+        '''
+        gets page that displays two datepickers one for start
+        date and the other for end date
+        '''
+        return render(request, 'reports/dates.html')
