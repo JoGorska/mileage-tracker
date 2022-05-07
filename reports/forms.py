e9ea5130a0fb54eb2ca48f323ddcd693cd13d38e
Joanna Gorska <gorska919@gmail.com>
2022-05-07 22:30:52 +0000

    Fix incorrect input type for date field

--- reports/forms.py
+++ reports/forms.py
@@ -4,31 +4,43 @@ from django import forms
 from django.conf import settings
 
 
+class DateInput(forms.DateInput):
+    '''
+    class that ads input_type date to to the widget
+    '''
+    input_type = 'date'
+
+
 class ReportingPeriodForm(forms.Form):
     '''
     form that renders start date and end date of the report
     '''
     start_date = forms.DateField(
+        input_formats=settings.DATE_INPUT_FORMATS,
         initial=datetime.date.today,
         widget=forms.DateInput(
-            format=settings.DATE_INPUT_FORMATS,
-            attrs={'class': 'mt-2'}),
+            attrs={'class': 'mt-2', 'type': 'date'}),
     )
     end_date = forms.DateField(
         input_formats=settings.DATE_INPUT_FORMATS,
         initial=datetime.date.today,
         widget=forms.DateInput(
-            format=settings.DATE_INPUT_FORMATS,
-            attrs={'class': 'mt-2'}))
 
+            attrs={'class': 'mt-2', 'type': 'date'}))
+
+    # class Meta:
+    #     fields = ('start_date', 'end_date',)
+    #     widgets = {
+    #         'date_picked': DateInput(),
+    #     }
     def clean_start_date(self):
         '''
         returns errors if the field is filled in incorrectly
         '''
-        self.cleaned_data['start_date']
-        if data != None and data > datetime.now():
+        data = self.cleaned_data['start_date']
+        if data > datetime.datetime.today:
             raise forms.ValidationError(
-                    """
-                    \'to\' date cannot be later than today.
-                    """)
+                """
+                \'to\' date cannot be later than today.
+                """)
 # clean the data
