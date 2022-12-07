810ae9e2e24054571ae94988ea4fa80eb8c96722
Joanna Gorska <gorska919@gmail.com>
2022-12-07 22:43:15 +0000

    add url for exporting excel

--- reports/urls.py
+++ reports/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.ReportingOptionsView.as_view(), name='reporting_options'),
     path('period/', views.ChoosePeriodView.as_view(), name='choose_period'),
     path('period/<slug:start_date>/<slug:end_date>', views.PeriodReportView.as_view(), name='period_report'),
+    path('excell/<slug:start_date>/<slug:end_date>', views.excelExportJourneys.as_view(), name='excel_journeys'),  # noqa
     path("date/", views.DatePickerView.as_view(), name="choose_date"),
     path("date/<slug:slug>/", views.DayReport.as_view(), name="day_report"),
 ]
